dfbe24b77e8d0533bbfae9b6b1c67c7b5da8cb9a
Markus Hjort <markus.hjort@gmail.com>
2017-02-14 18:28:33 +0200

    Route AWS Scheduled Events to given route

--- src/ring/middleware/apigw.clj
+++ src/ring/middleware/apigw.clj
@@ -4,12 +4,19 @@
 (defn- generate-query-string [params]
   (clojure.string/join "&" (map (fn [[k v]] (str (URLEncoder/encode (name k)) "=" (URLEncoder/encode v))) params)))
 
-(defn wrap-apigw-lambda-proxy [handler]
-  (fn [apigw-request]
-    (let [ring-request {:uri (:path apigw-request)
-                        :query-string (generate-query-string (:queryStringParameters apigw-request))
-                        :request-method :get}
-          response (handler ring-request)]
-      {:statusCode (:status response)
-       :headers (:headers response)
-       :body (:body response)})))
+(defn wrap-apigw-lambda-proxy
+  ([handler] (wrap-apigw-lambda-proxy handler {}))
+  ([handler {:keys [scheduled-event-route]}]
+   (fn [apigw-request]
+     (let [scheduled-event? (= "Scheduled Event" (:detail-type apigw-request))
+           ring-request (if scheduled-event?
+                          {:uri scheduled-event-route
+                           :query-string ""
+                           :request-method :get}
+                          {:uri (:path apigw-request)
+                           :query-string (generate-query-string (:queryStringParameters apigw-request))
+                           :request-method :get})
+           response (handler ring-request)]
+       {:statusCode (:status response)
+        :headers (:headers response)
+        :body (:body response)}))))
--- test/ring/middleware/apigw_test.clj
+++ test/ring/middleware/apigw_test.clj
@@ -1,6 +1,23 @@
 (ns ring.middleware.apigw-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [cheshire.core :refer [parse-string]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.json :refer [wrap-json-response]]
             [ring.middleware.apigw :refer [wrap-apigw-lambda-proxy]]))
+
+(defroutes app-routes
+  (GET "/warmup" request {:status 200 :body "warmup"})
+  (route/not-found {:status 404 :body {:errors "Route not found"}}))
+
+(def ring-routes
+  (-> app-routes
+      wrap-keyword-params
+      wrap-params
+      wrap-json-response))
 
 (deftest query-string-is-url-encoded
   (let [handler (fn [request] {:body request :status 200 :headers nil})
@@ -10,3 +27,10 @@
                                          (keyword "foo[]") "bar"}}
         query-string (get-in (app request) [:body :query-string])]
     (is (= query-string "text=hello%2C+world%21&foo%5B%5D=bar"))))
+
+(deftest scheduled-event-is-handled
+  (let [app (wrap-apigw-lambda-proxy ring-routes {:scheduled-event-route "/warmup"})
+        result (app (-> (io/resource "scheduled-event.json")
+                        (slurp)
+                        (parse-string true)))]
+    (is (= {:statusCode 200 :headers {} :body "warmup"} result))))
