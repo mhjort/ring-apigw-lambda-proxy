627d80bc9367581158ff14ae63013104d73d31a5
Markus Hjort <markus.hjort@gmail.com>
2017-02-14 19:30:19 +0200

    Release version 0.0.3 (Scheduled Event support)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-apigw-lambda-proxy "0.0.2"
+(defproject ring-apigw-lambda-proxy "0.0.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
